f46d4ab0a68cb135be3e559d41da97b7875201fd
Xihan <fxhhhh@berkeley.edu>
2021-06-23 15:26:23 +0800

    lab01: Completed LeapYear.java

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -1,6 +1,6 @@
 /**
  * Class that determines whether or not a year is a leap year.
- * @author YOUR NAME HERE
+ * @author Xihan Fu
  */
 public class LeapYear {
 
@@ -10,6 +10,7 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year) {
         // TODO: Fill in this method.
+        return year % 4 == 0 & year % 100 != 0 | year % 400 == 0;
     }
 
     /** Calls isLeapYear to print correct statement. */

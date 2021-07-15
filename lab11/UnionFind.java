7111b6dc36633e08878c210c1c60bb4a1a535187
Xihan <fxhhhh@berkeley.edu>
2021-07-15 16:19:03 +0800

    finished lab11

--- lab11/UnionFind.java
+++ lab11/UnionFind.java
@@ -1,29 +1,48 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class UnionFind {
 
     /* TODO: Add instance variables here. */
+    private int[] unionArray;
 
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
         // TODO: YOUR CODE HERE
+        unionArray = new int[N];
+        for (int i = 0; i < N; i++) {
+            unionArray[i] = -1;
+        }
+
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        if (unionArray[v] >=0) {
+            return sizeOf(unionArray[v]);
+        } else {
+            return -unionArray[v];
+        }
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        if (unionArray[v] > 0) {
+            return unionArray[v];
+        }
+        return sizeOf(v);
     }
 
     /* Returns true if nodes V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO: YOUR CODE HERE
+        if (parent(v1) == parent(v2) && unionArray[v1] != -1) {
+            return true;
+        }
         return false;
     }
 
@@ -32,7 +51,15 @@ public class UnionFind {
        function, throw an IllegalArgumentException. */
     public int find(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        Set<Integer> a = new HashSet<>();
+        while (unionArray[v] >= 0) {
+            a.add(unionArray[v]);
+            v = unionArray[v];
+        }
+        for (Integer i : a) {
+            unionArray[i] = v;
+        }
+        return v;
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -42,5 +69,26 @@ public class UnionFind {
        already connected should not change the structure. */
     public void union(int v1, int v2) {
         // TODO: YOUR CODE HERE
+        int root1 = find(v1);
+        int root2 = find(v2);
+        if (root1 == root2) {
+            return;
+        }
+        if (unionArray[root1] < unionArray[root2]) {
+            unionArray[root1] = unionArray[root1] + unionArray[root2];
+            unionArray[root2]=root1;
+        }else {
+            unionArray[root2] = unionArray[root1] + unionArray[root2];
+            unionArray[root1]=root2;
+        }
+    }
+
+    public static void main(String[] args) {
+        UnionFind d=new UnionFind(5);
+        d.union(1,0);
+        d.union(3,2);
+        d.union(2,0);//大小
+        System.out.println(d.sizeOf(3));
+        System.out.println(d.find(1));
     }
 }
